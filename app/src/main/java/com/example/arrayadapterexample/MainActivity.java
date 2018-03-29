f23a76341fa0e7797f79588381e53e02bfcefd8a
frederiek de wolf <frederiek.dewolf@avondschool.hogent.be>
2018-03-29 16:33:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/arrayadapterexample/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.arrayadapterexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //String[] myStringCollection = {"Hicham", "Robby", "Maria", "Tim", "Johan"};
+        final ArrayList<String> myStringCollection = new ArrayList<>();
+        myStringCollection.add("Hicham");
+        myStringCollection.add("Robby");
+        myStringCollection.add("Maria");
+        myStringCollection.add("Tim");
+        myStringCollection.add("Johan");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringCollection);
+
+        ListView listView = findViewById(R.id.listview1);
+        listView.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener((View v) -> {
+                    if (myStringCollection.size() > 0) {
+                        myStringCollection.remove(0);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+        );
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Object name = listView.getItemAtPosition(position);
+                Snackbar.make(v, "Hello " + name.toString(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
